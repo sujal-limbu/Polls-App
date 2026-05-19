3a256c5a9e8c52d7c85e2217af478d2135233ff2
Sujal <sujallimbu31@gmail.com>
2026-05-19 21:25:54 +0545

    Added Few Fixes

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('' , views.home )
+    path('' , views.home , name = 'index'),
+    path('<int:question_id>/' , views.detail , name = 'detail'),
+    path('<int:question_id>/results' , views.results , name = 'results'),
+    path('<int:question_id>/votes' , views.vote , name = 'vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,21 @@
 from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from .models import Question
 
 def home(request):
-    return HttpResponse("Hello World")
+    latest_question = Question.objects.order_by('-pub_date')[:5]
+    context = {"latest_question":latest_question}
+    return render(request , 'polls/index.html' , context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request , 'polls/detail.html' , {'question':question})
+    
+
+def results(request, question_id):
+    response = "You are looking at results %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    response = "You are voting on question %s"
+    return HttpResponse(response % question_id)
