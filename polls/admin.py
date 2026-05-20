4497f00458f34aed24953c69fa9e1ca2ea642acc
Sujal <sujallimbu31@gmail.com>
2026-05-20 12:42:02 +0545

    Pushed changed models

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Question,Choices
+from .models import Question,Choice
 
 # Register your models here.
 admin.site.register(Question)
-admin.site.register(Choices)
+admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -11,16 +11,14 @@ class Question(models.Model):
         return self.question_text
     
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime().timedelta(days=1)
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-class Choices(models.Model):
 
-    question = models.ForeignKey(Question , on_delete= models.CASCADE)
-    choices_text = models.CharField(max_length=100)
+class Choice(models.Model):
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=100)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choices_text
-
-
-
+        return self.choice_text
